d0d2438f4f285ece073fea4553eb4daaef6835a4
Richard Westmoreland <kegtrack@gmail.com>
2019-12-11 09:48:34 -0600

    homepage to heroku app

--- src/components/WeatherDetails/WeatherDetails.jsx
+++ src/components/WeatherDetails/WeatherDetails.jsx
@@ -1,43 +1,25 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
-import StationDetails from '../StationDetails/StationDetails'
-import MetarCard from './MetarCard'
-import TafCard from './TafCard'
-import './WeatherDetails.css'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import StationDetails from "../StationDetails/StationDetails";
+import MetarCard from "./MetarCard";
+import TafCard from "./TafCard";
+import "./WeatherDetails.css";
 
-import { getIcaoWeather } from '../../actions/icaoWeatherActions'
-import { getIcaoTaf } from '../../actions/icaoTafActions'
-import { getIcaoStation } from '../../actions/icaoStationActions'
-import { getStationTimestamp } from '../../actions/stationTimestampActions'
+import { getIcaoWeather } from "../../actions/icaoWeatherActions";
+import { getIcaoTaf } from "../../actions/icaoTafActions";
+import { getIcaoStation } from "../../actions/icaoStationActions";
+import { getStationTimestamp } from "../../actions/stationTimestampActions";
 
 class WeatherDetails extends Component {
-  // const [icao, setIcao] = useState(match.params.icao)
-  // const [weatherData, setWeatherData] = useState(null)
-  // const [tafData, setTafData] = useState(null)
-
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     icao: props.match.params.icao
-  //   }
-  // }
-
   componentDidMount() {
-    const icao = this.props.match.params.icao
-    console.log('resetting the values for a new city')
-    this.props.getIcaoWeather(icao)
-    this.props.getIcaoTaf(icao)
-    this.props.getIcaoStation(icao)
-    this.props.getStationTimestamp(icao)
+    const icao = this.props.match.params.icao;
+    console.log("resetting the values for a new city");
+    this.props.getIcaoWeather(icao);
+    this.props.getIcaoTaf(icao);
+    this.props.getIcaoStation(icao);
+    this.props.getStationTimestamp(icao);
   }
-
-  // const api_key = process.env.REACT_APP_API_KEY
-
-  // const [stationDataLoading, setStationDataLoading] = useState(true)
-  // const [stationTimestampLoading, setStationTimestampLoading] = useState(true)
-  // const [stationData, setStationData] = useState(null)
-  // const [stationTimestamp, setStationTimestamp] = useState(null)
 
   static propTypes = {
     getIcaoWeather: PropTypes.func.isRequired,
@@ -50,63 +32,7 @@ class WeatherDetails extends Component {
     tafData: PropTypes.object,
     stationData: PropTypes.object,
     stationTimestamp: PropTypes.object
-  }
-
-  // useEffect(() => {
-  //   console.log('resetting the values for a new city')
-  //   getIcaoWeather(icao)
-  //   fetchWeather(icao)
-  //   fetchTaf(icao)
-  //   fetchStation(icao)
-  //   fetchStationTimestamp(icao)
-  // }, [icao])
-
-  // fetchWeather = async icao => {
-  //   const data = await fetch(`https://api.checkwx.com/metar/${icao}/decoded`, {
-  //     headers: { 'X-API-Key': api_key }
-  //   })
-
-  //   const jsonData = await data.json()
-
-  //   setWeatherData(jsonData.data[0])
-
-  //   console.log(weatherData)
-  // }
-
-  // fetchTaf = async icao => {
-  //   const data = await fetch(`https://api.checkwx.com/taf/${icao}/decoded`, {
-  //     headers: { 'X-API-Key': api_key }
-  //   })
-
-  //   const jsonData = await data.json()
-
-  //   setTafData(jsonData.data[0])
-  // }
-
-  // fetchStation = async icao => {
-  //   const data = await fetch(`https://api.checkwx.com/station/${icao}/`, {
-  //     headers: { 'X-API-Key': api_key }
-  //   })
-
-  //   const jsonData = await data.json()
-
-  //   setStationData(jsonData.data[0])
-
-  //   setStationDataLoading(false)
-  // }
-
-  //  fetchStationTimestamp = async icao => {
-  //     const data = await fetch(
-  //       `https://api.checkwx.com/station/${icao}/timestamp`,
-  //       { headers: { 'X-API-Key': api_key } }
-  //     )
-
-  //     const jsonData = await data.json()
-
-  //     setStationTimestamp(jsonData.data[0])
-
-  //     setStationTimestampLoading(false)
-  //   }
+  };
 
   render() {
     const {
@@ -114,30 +40,26 @@ class WeatherDetails extends Component {
       stationTimestampLoading,
       weatherData,
       tafData
-    } = this.props
+    } = this.props;
     return (
       <div
         className={
           stationDataLoading && stationTimestampLoading
-            ? 'loading-wrapper'
-            : 'weather-details-wrapper'
+            ? "loading-wrapper"
+            : "weather-details-wrapper"
         }
       >
         {!stationDataLoading && !stationTimestampLoading ? (
-          <StationDetails
-          /*stationData={stationData[0]}
-            stationTimestamp={stationTimestamp[0]}
-            icao={this.props.match.params.icao}*/
-          />
+          <StationDetails />
         ) : (
           <div>
-            <i className='load-spinner fas fa-asterisk fa-spin fa-3x'></i>
+            <i className="load-spinner fas fa-asterisk fa-spin fa-3x"></i>
           </div>
         )}
-        <div className='divider'></div>
+        <div className="divider"></div>
         {!stationDataLoading && !stationTimestampLoading ? (
           weatherData ? (
-            <MetarCard /*data={weatherData[0]}*/ />
+            <MetarCard />
           ) : (
             <div></div>
           )
@@ -146,7 +68,7 @@ class WeatherDetails extends Component {
         )}
         {!stationDataLoading && !stationTimestampLoading ? (
           tafData ? (
-            <TafCard /*data={tafData[0]}*/ />
+            <TafCard />
           ) : (
             <div></div>
           )
@@ -154,7 +76,7 @@ class WeatherDetails extends Component {
           <div></div>
         )}
       </div>
-    )
+    );
   }
 }
 
@@ -165,9 +87,11 @@ const mapStateToProps = state => ({
   stationData: state.icaoStation.stationData,
   stationDataLoading: state.icaoStation.isLoading,
   stationTimestampLoading: state.stationTimestamp.isLoading
-})
+});
 
-export default connect(
-  mapStateToProps,
-  { getIcaoWeather, getIcaoTaf, getIcaoStation, getStationTimestamp }
-)(WeatherDetails)
+export default connect(mapStateToProps, {
+  getIcaoWeather,
+  getIcaoTaf,
+  getIcaoStation,
+  getStationTimestamp
+})(WeatherDetails);
